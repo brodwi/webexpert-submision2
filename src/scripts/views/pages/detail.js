07e2dec9dc02421ee45af216cf319cd6e1b4fb40
brodwi <121082176+brodwi@users.noreply.github.com>
2023-05-24 11:45:10 +0700

    detail

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,13 +1,19 @@
+import UrlParser from '../../routes/url-parser';
+import restaurantSource from '../../data/restaurant-source';
+import { createrestaurantsDetailTemplate } from '../templates/template-creator';
 const Detail = {
     async render() {
       return `
           <div id="detailRestaurant" class="detailRestaurant"></div>
-          <div id="likeButtonContainer"></div>
           `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await restaurantSource.detailRestaurant(url.id);
+    const restaurantContainer = document.querySelector('#detailRestaurant');
+    restaurantContainer.innerHTML = createrestaurantsDetailTemplate(restaurant);
+
   },
 };
 
