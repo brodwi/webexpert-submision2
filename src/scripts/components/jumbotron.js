504d0b8a2419578e45349f4276bbbd9d4d509b93
brodwi <121082176+brodwi@users.noreply.github.com>
2023-05-24 13:37:40 +0700

    workbox

--- src/scripts/components/jumbotron.js
+++ src/scripts/components/jumbotron.js
@@ -7,7 +7,7 @@ class Jumbotran extends HTMLElement {
       this.innerHTML = `
       <div class="jumbotron">
       <div class="jumbotron-inner">
-        <h1 class="jumbotron-title">Brodwi Restaurant</h1>
+        <h1 class="jumbotron-title">Restaurant</h1>
         <p class="jumbotron-subtitle">
           Restaurant Harga Kaki Lima, Rasa Bintang Lima.
         </p>
